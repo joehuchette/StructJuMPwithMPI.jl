e332654e9cbaafdcc93af0a43cfd751127526ed3
Joey Huchette <joehuchette@gmail.com>
2018-08-02 11:26:33 -0400

    Old StructJuMP code that uses MPI

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 [![Coverage Status](https://coveralls.io/repos/joehuchette/StructJuMPwithMPI.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/joehuchette/StructJuMPwithMPI.jl?branch=master)
 
 [![codecov.io](http://codecov.io/github/joehuchette/StructJuMPwithMPI.jl/coverage.svg?branch=master)](http://codecov.io/github/joehuchette/StructJuMPwithMPI.jl?branch=master)
+
+A package that allows you to use StructJuMP with MPI to generate models in parallel. To use, load both packages, and pass the ``mpi_wrapper`` keyword argument to the ``StructuredModel`` constructor:
+
+```jl
+using JuMP, StructJuMP, StructJuMPwithMPI
+model = StructuredModel(mpi_wrapper=mpi_wrapper)
+```
--- src/StructJuMPwithMPI.jl
+++ src/StructJuMPwithMPI.jl
@@ -1,5 +1,68 @@
 module StructJuMPwithMPI
 
-# package code goes here
+using StructJuMP
+using MPI
+
+export mpi_wrapper
+
+type MPIWrapper
+    comm::MPI.Comm
+    init::Function
+
+    function MPIWrapper()
+        instance = new(MPI.Comm(-1))
+        finalizer(instance, freeMPIWrapper)
+
+        instance.init = function(ucomm::MPI.Comm)
+            if MPI.Initialized() && ucomm.val == -1
+                instance.comm = MPI.COMM_WORLD
+            elseif !MPI.Initialized()
+                MPI.Init()
+                instance.comm = MPI.COMM_WORLD
+            elseif MPI.Initialized() && ucomm.val != -1
+                instance.comm = ucomm
+            elseif MPI.Finalized()
+                error("MPI is already finalized!")
+            else
+                #doing nothing
+            end
+        end
+        return instance
+    end
+end
+function freeMPIWrapper(instance::MPIWrapper)
+    if isdefined(:MPI) && MPI.Initialized() && !MPI.Finalized()
+        MPI.Finalize()
+    end
+end
+
+const mpi_wrapper = MPIWrapper()
+
+function getMyRank()
+    myrank = 0
+    mysize = 1
+    if isdefined(:MPI) && MPI.Initialized() && !MPI.Finalized()
+        comm = MPI.COMM_WORLD
+        mysize = MPI.Comm_size(comm)
+        myrank = MPI.Comm_rank(comm)
+    end
+    return myrank, mysize
+end
+
+function StructJuMP.getProcIdxSet(mpi_wrapper::MPIWrapper, numScens::Integer)
+    mysize = 1;
+    myrank = 0;
+    if isdefined(:MPI) == true && MPI.Initialized() == true
+        comm = MPI.COMM_WORLD
+        mysize = MPI.Comm_size(comm)
+        myrank = MPI.Comm_rank(comm)
+    end
+    # Why don't we just take a round-and-robin?
+    proc_idx_set = Int[];
+    for s = myrank:mysize:(numScens-1)
+        push!(proc_idx_set, s+1);
+    end
+    return proc_idx_set;
+end
 
 end # module
